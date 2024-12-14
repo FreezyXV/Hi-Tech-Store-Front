de7da5f62ff7dd61a32b8837e7fcd5542e44cd30
FreezyXV <i_841@mail.ru>
2024-12-14 13:57:35 +0100

    vite.config update

--- vite.config.js
+++ vite.config.js
@@ -2,22 +2,23 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import dotenv from 'dotenv';
 
+// Load environment variables from a .env file
 dotenv.config();
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Hi-Tech-Store-Front/", // Set the base path for GitHub Pages
+  base: process.env.VERCEL ? "/" : "/Hi-Tech-Store-Front/", // Use "/" for Vercel, GitHub Pages base for GitHub
   server: {
     port: 3000,
   },
   build: {
     rollupOptions: {
-      // Remove 'react-toastify' from external dependencies
+      // Customize Rollup options if needed
     },
   },
   define: {
     'process.env': {
-      VITE_API_URL: JSON.stringify(process.env.VITE_API_URL),
+      VITE_API_URL: JSON.stringify(process.env.VITE_API_URL_VERCEL || process.env.VITE_API_URL), // Use VERCEL API URL if available
       VITE_STRIPE_PUBLISHABLE_KEY: JSON.stringify(process.env.VITE_STRIPE_PUBLISHABLE_KEY),
     },
   },
