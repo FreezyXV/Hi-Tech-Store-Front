b4a8dc7b5079d60bc7a7f551afb35b80ea7fee98
FreezyXV <i_841@mail.ru>
2024-12-14 10:08:54 +0100

    Vite.config Update

--- vite.config.js
+++ vite.config.js
@@ -2,22 +2,25 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import dotenv from 'dotenv';
 
+// Load environment variables
 dotenv.config();
 
 export default defineConfig({
   plugins: [react()],
+  base: "/Hi-Tech-Store-Front/", // Set the base path for GitHub Pages
   server: {
-
+    // Add custom server options if needed (e.g., port, proxy)
+    port: 3000,
   },
   build: {
     rollupOptions: {
-      external: ['react-toastify', 'react-toastify/dist/ReactToastify.css'],
+      external: ['react-toastify', 'react-toastify/dist/ReactToastify.css'], // External dependencies
     },
   },
   define: {
     'process.env': {
-      VITE_API_URL: JSON.stringify(process.env.VITE_API_URL),
-      VITE_STRIPE_PUBLISHABLE_KEY: JSON.stringify(process.env.VITE_STRIPE_PUBLISHABLE_KEY),
+      VITE_API_URL: JSON.stringify(process.env.VITE_API_URL), // API URL for the front-end
+      VITE_STRIPE_PUBLISHABLE_KEY: JSON.stringify(process.env.VITE_STRIPE_PUBLISHABLE_KEY), // Stripe Key
     },
   },
 });
