971381e92b6424da49d922858419de06dae03235
FreezyXV <i_841@mail.ru>
2024-12-16 11:26:45 +0100

    vite.config updated rules

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,24 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import dotenv from "dotenv";
 
-dotenv.config();
+// Définir le chemin de base en fonction du mode de build
+export default defineConfig(({ mode }) => {
+  let base = "/"; // Par défaut pour Vercel
 
-export default defineConfig({
-  plugins: [react()],
-  base: process.env.VERCEL ? "/" : "/Hi-Tech-Store-Front/",
-  server: {
-    port: 3000,
-  },
-  define: {
-    "process.env": {
-      VITE_API_URL: JSON.stringify(
-        process.env.VITE_API_URL_LOCAL || process.env.VITE_API_URL
-      ),
-      VITE_STRIPE_PUBLISHABLE_KEY: JSON.stringify(
-        process.env.VITE_STRIPE_PUBLISHABLE_KEY
-      ),
+  if (mode === "gh-pages") {
+    // Remplacez 'Hi-Tech-Store-Front' par le nom exact de votre dépôt si nécessaire
+    base = "/Hi-Tech-Store-Front/";
+  }
+
+  return {
+    plugins: [react()],
+    base,
+    server: {
+      port: 3000,
     },
-  },
+    build: {
+      outDir: "dist",
+    },
+  };
 });
-
